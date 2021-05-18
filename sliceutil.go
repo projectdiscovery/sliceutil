76c506636b6973ecfba7e8e71949403489e29647
Mzack9999 <mzack9999@protonmail.com>
2021-05-18 19:58:55 +0200

    adding helpers

---
+++ sliceutil.go
@@ -0,0 +1,14 @@
+package sliceutil
+
+func PruneEmptyStrings(v []string) []string {
+	return PruneEqual(v, "")
+}
+
+func PruneEqual(v []string, equalTo string) (r []string) {
+	for i := range v {
+		if v[i] == equalTo {
+			r = append(r, v[i])
+		}
+	}
+	return r
+}
